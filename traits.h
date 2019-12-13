c8cf2f96ec7950ae3ab2a5e1fe412af021d9e31a
Lutz Freitag <lutz@gottliebtfreitag.de>
2019-12-13 13:58:13 +0700

    cleanup in the traits

--- traits.h
+++ traits.h
@@ -8,35 +8,20 @@ namespace traits
 {
 
 template <typename T>
-struct is_map {
-private:
-	template <typename KT, typename MT>
-	static std::true_type test(std::map<KT, MT>);
-
-	template <typename...>
-	static std::false_type test(...);
-public:
-	using type = decltype(test(std::declval<T>()));
-	enum { value = type::value };
-};
-template<typename T>
-inline constexpr bool is_map_v = is_map<T>::value;
+struct is_map : std::false_type {};
+template <typename... Ts>
+struct is_map<std::map<Ts...>> : std::true_type {};
+template <typename... Ts>
+struct is_map<std::unordered_map<Ts...>> : std::true_type {};
+template<typename... Ts>
+inline constexpr bool is_map_v = is_map<Ts...>::value;
 
 template <typename T>
-struct is_pair {
-private:
-	template <typename KT, typename MT>
-	static std::true_type test(std::pair<KT, MT>);
-
-	template <typename...>
-	static std::false_type test(...);
-public:
-	using type = decltype(test(std::declval<T>()));
-	enum { value = type::value };
-};
-template<typename T>
-inline constexpr bool is_pair_v = is_pair<T>::value;
-
+struct is_pair : std::false_type {};
+template <typename... Ts>
+struct is_pair<std::pair<Ts...>> : std::true_type {};
+template<typename... Ts>
+inline constexpr bool is_pair_v = is_pair<Ts...>::value;
 
 template <typename T, typename Arg1>
 struct has_serialize_function {
@@ -50,7 +35,6 @@ public:
 	using type = decltype(test<T>(int(0)));
 	enum { value = type::value };
 };
-
 
 template <typename T, typename Arg>
 inline constexpr bool has_serialize_function_v = has_serialize_function<T, Arg>::value;
